12c0d3e256256cb53d35db12612276a91b3761f8
Abdullahi Abdul Kabir <abdullahiabdulkabir1@gmail.com>
2025-01-22 09:05:57 +0100

    fix route

--- src/bulma/routes/administration/categories.js
+++ src/bulma/routes/administration/categories.js
@@ -1,11 +1,14 @@
-const CategoryIndex = () => import('../../pages/administration/categories/Index.vue');
+import routeImporter from '@enso-ui/ui/src/modules/importers/routeImporter';
+
+const routes = routeImporter(require.context('./categories', false, /.*\.js$/));
+const RouterView = () => import('@enso-ui/ui/src/bulma/pages/Router.vue');
 
 export default {
-    name: 'administration.categories.index',
-    path: 'categories',
-    component: CategoryIndex,
+    path: '/categories',
+    component: RouterView,
     meta: {
         breadcrumb: 'categories',
-        title: 'Categories',
+        route: 'administration.categories.index',
     },
+    children: routes,
 };
